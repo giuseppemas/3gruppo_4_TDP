9a6b4bcba2b7229f74f4096308b72a4058354f72
BonnieCharlie <BonnieCharlie@users.noreply.github.com>
2017-12-22 12:58:57 +0100

    Aggiunti due file: MyGraph e bridge
    Ho aggiunto anche MyGraph perchè mi serviva ricevere in input la classe per l'esercizio 2

---
+++ pkg_1/MyGraph.py
@@ -0,0 +1,14 @@
+from TdP_collections.graphs.graph import Graph
+
+class MyGraph (Graph):
+
+    def __init__(self):
+        super(MyGraph, self).__init__()
+
+
+    def min_vertex_cover(self):
+        pass
+
+
+    def greedy_vertex_cover(self):
+        pass
---
+++ pkg_2/bridge.py
@@ -0,0 +1,9 @@
+from pkg_1.MyGraph import MyGraph
+
+"""
+Preso in input un oggetto MyGraph G,
+restituisce un bridge di G
+oppure None se il grafo è biconnesso
+"""
+def bridge(G):
+    pass
