cdb455c4f2d96a3e62e6d8ca3a0867e9f9aec64a
BonnieCharlie <BonnieCharlie@users.noreply.github.com>
2018-01-04 18:02:14 +0100

    Corrette cose

--- test/testVertexCover.py
+++ test/testVertexCover.py
@@ -54,59 +54,7 @@ print("L'algoritmo Greedy è", int(t_MVC/t_GVC), "volte piu' veloce di quello Mi
 print("\n\n")
 
 
-print("SECONDO ESEMPIO: Grafo non diretto non pesato con 34 vertici e 78 archi")
-graph2 = MyGraph()
-vert = []
-for i in range (1,35):
-    vert.append(graph2.insert_vertex(i))
-print("Vertici: [")
-for v in graph2.vertices():
-    print(v, end=", ")
-print("]\n\n")
-
-in_file = open("zachary_club.txt","r")              #lettura da file
-testo = in_file.read()
-in_file.close()
-lettura = testo.splitlines()
-lettura = list(lettura)
-lenLettura = len(lettura)
-i = 2
-while i < lenLettura:
-    endpoints = lettura[i].split(" ")               #inserisce i 2 estremi in un array di 2 elementi
-    j = 0
-    while vert[j]._element != int(endpoints[0]):
-        j = j+1
-    z = 0
-    while vert[z]._element != int(endpoints[1]):
-        z = z + 1
-    graph2.insert_edge(vert[j],vert[z])
-    i = i+1
-print("Archi: [")
-for e in graph2.edges():
-    print(e, end=", ")
-print("]\n\n")
-
-print("Greedy vertex cover: ")
-inizio_GVC = time.clock()
-listGVC = graph2.greedy_vertex_cover()
-fine_GVC = time.clock()
-t_GVC = fine_GVC - inizio_GVC
-for elem in listGVC:
-    print(elem, end=", ")
-print("\nMin vertex cover: ")
-inizio_MVC = time.clock()
-listMVC = graph2.min_vertex_cover()
-fine_MVC = time.clock()
-t_MVC = fine_MVC - inizio_MVC
-for elem in listMVC:
-    print(elem, end=", ")
-print("\n\nGREEDY --> Dim =", len(listGVC), ", Time =", t_GVC, "s\n")
-print("MIN --> Dim =", len(listMVC), ", Time =", t_MVC, "s\n")
-print("La dimensione dell'algoritmo Greedy è di", len(listGVC)-len(listMVC), "vertici in più rispetto all'algoritmo Min.\n")
-print("L'algoritmo Greedy è", int(t_MVC/t_GVC), "volte piu' veloce di quello Min.\n")
-print("\n\n")
-
-print("TERZO ESEMPIO: Grafo non diretto non pesato con 12 vertici e 17 archi")
+print("SECONDO ESEMPIO: Grafo non diretto non pesato con 12 vertici e 17 archi")
 graph3 = MyGraph()
 vertex1 = graph3.insert_vertex(1)
 vertex2 = graph3.insert_vertex(2)
@@ -169,7 +117,7 @@ print("L'algoritmo Greedy è", int(t_MVC/t_GVC), "volte piu' veloce di quello Mi
 print("\n\n")
 
 
-print("QUARTO ESEMPIO: Grafo non diretto non pesato con 15 vertici e 51 archi")
+print("TERZO ESEMPIO: Grafo non diretto non pesato con 15 vertici e 51 archi")
 graph4 = MyGraph()
 vert2 = []
 for i in range (1,16):
@@ -222,7 +170,7 @@ print("L'algoritmo Greedy è", int(t_MVC/t_GVC), "volte piu' veloce di quello Mi
 print("\n\n")
 
 
-print("QUINTO ESEMPIO: Grafo non diretto non pesato con 15 vertici e 51 archi")
+print("QUARTO ESEMPIO: Grafo non diretto non pesato con 20 vertici e 51 archi")
 graph5 = MyGraph()
 vert5 = []
 R = 20
@@ -253,6 +201,168 @@ for elem in listGVC:
 print("\nMin vertex cover: ")
 inizio_MVC = time.clock()
 listMVC = graph5.min_vertex_cover()
+fine_MVC = time.clock()
+t_MVC = fine_MVC - inizio_MVC
+for elem in listMVC:
+    print(elem, end=", ")
+print("\n\nGREEDY --> Dim =", len(listGVC), ", Time =", t_GVC, "s\n")
+print("MIN --> Dim =", len(listMVC), ", Time =", t_MVC, "s\n")
+print("La dimensione dell'algoritmo Greedy è di", len(listGVC)-len(listMVC), "vertici in più rispetto all'algoritmo Min.\n")
+print("L'algoritmo Greedy è", int(t_MVC/t_GVC), "volte piu' veloce di quello Min.\n")
+print("\n\n")
+
+
+
+print("QUINTO ESEMPIO: Grafo non diretto non pesato con 34 vertici e 50 archi")
+graph2 = MyGraph()
+vert = []
+for i in range (1,35):
+    vert.append(graph2.insert_vertex(i))
+print("Vertici: [")
+for v in graph2.vertices():
+    print(v, end=", ")
+print("]\n\n")
+
+in_file = open("zachary_club.txt","r")              #lettura da file
+testo = in_file.read()
+in_file.close()
+lettura = testo.splitlines()
+lettura = list(lettura)
+lenLettura = len(lettura)
+i = 2
+while i < 50:
+    endpoints = lettura[i].split(" ")               #inserisce i 2 estremi in un array di 2 elementi
+    j = 0
+    while vert[j]._element != int(endpoints[0]):
+        j = j+1
+    z = 0
+    while vert[z]._element != int(endpoints[1]):
+        z = z + 1
+    graph2.insert_edge(vert[j],vert[z])
+    i = i+1
+print("Archi: [")
+for e in graph2.edges():
+    print(e, end=", ")
+print("]\n\n")
+
+print("Greedy vertex cover: ")
+inizio_GVC = time.clock()
+listGVC = graph2.greedy_vertex_cover()
+fine_GVC = time.clock()
+t_GVC = fine_GVC - inizio_GVC
+for elem in listGVC:
+    print(elem, end=", ")
+print("\nMin vertex cover: ")
+inizio_MVC = time.clock()
+listMVC = graph2.min_vertex_cover()
+fine_MVC = time.clock()
+t_MVC = fine_MVC - inizio_MVC
+for elem in listMVC:
+    print(elem, end=", ")
+print("\n\nGREEDY --> Dim =", len(listGVC), ", Time =", t_GVC, "s\n")
+print("MIN --> Dim =", len(listMVC), ", Time =", t_MVC, "s\n")
+print("La dimensione dell'algoritmo Greedy è di", len(listGVC)-len(listMVC), "vertici in più rispetto all'algoritmo Min.\n")
+print("L'algoritmo Greedy è", int(t_MVC/t_GVC), "volte piu' veloce di quello Min.\n")
+print("\n\n")
+
+
+
+print("QUINTO ESEMPIO: Grafo non diretto non pesato con 34 vertici e 60 archi")
+graph2 = MyGraph()
+vert = []
+for i in range (1,35):
+    vert.append(graph2.insert_vertex(i))
+print("Vertici: [")
+for v in graph2.vertices():
+    print(v, end=", ")
+print("]\n\n")
+
+in_file = open("zachary_club.txt","r")              #lettura da file
+testo = in_file.read()
+in_file.close()
+lettura = testo.splitlines()
+lettura = list(lettura)
+lenLettura = len(lettura)
+i = 2
+while i < 60:
+    endpoints = lettura[i].split(" ")               #inserisce i 2 estremi in un array di 2 elementi
+    j = 0
+    while vert[j]._element != int(endpoints[0]):
+        j = j+1
+    z = 0
+    while vert[z]._element != int(endpoints[1]):
+        z = z + 1
+    graph2.insert_edge(vert[j],vert[z])
+    i = i+1
+print("Archi: [")
+for e in graph2.edges():
+    print(e, end=", ")
+print("]\n\n")
+
+print("Greedy vertex cover: ")
+inizio_GVC = time.clock()
+listGVC = graph2.greedy_vertex_cover()
+fine_GVC = time.clock()
+t_GVC = fine_GVC - inizio_GVC
+for elem in listGVC:
+    print(elem, end=", ")
+print("\nMin vertex cover: ")
+inizio_MVC = time.clock()
+listMVC = graph2.min_vertex_cover()
+fine_MVC = time.clock()
+t_MVC = fine_MVC - inizio_MVC
+for elem in listMVC:
+    print(elem, end=", ")
+print("\n\nGREEDY --> Dim =", len(listGVC), ", Time =", t_GVC, "s\n")
+print("MIN --> Dim =", len(listMVC), ", Time =", t_MVC, "s\n")
+print("La dimensione dell'algoritmo Greedy è di", len(listGVC)-len(listMVC), "vertici in più rispetto all'algoritmo Min.\n")
+print("L'algoritmo Greedy è", int(t_MVC/t_GVC), "volte piu' veloce di quello Min.\n")
+print("\n\n")
+
+
+
+print("OTTAVO ESEMPIO: Grafo non diretto non pesato con 34 vertici e 78 archi")
+graph2 = MyGraph()
+vert = []
+for i in range (1,35):
+    vert.append(graph2.insert_vertex(i))
+print("Vertici: [")
+for v in graph2.vertices():
+    print(v, end=", ")
+print("]\n\n")
+
+in_file = open("zachary_club.txt","r")              #lettura da file
+testo = in_file.read()
+in_file.close()
+lettura = testo.splitlines()
+lettura = list(lettura)
+lenLettura = len(lettura)
+i = 2
+while i < lenLettura:
+    endpoints = lettura[i].split(" ")               #inserisce i 2 estremi in un array di 2 elementi
+    j = 0
+    while vert[j]._element != int(endpoints[0]):
+        j = j+1
+    z = 0
+    while vert[z]._element != int(endpoints[1]):
+        z = z + 1
+    graph2.insert_edge(vert[j],vert[z])
+    i = i+1
+print("Archi: [")
+for e in graph2.edges():
+    print(e, end=", ")
+print("]\n\n")
+
+print("Greedy vertex cover: ")
+inizio_GVC = time.clock()
+listGVC = graph2.greedy_vertex_cover()
+fine_GVC = time.clock()
+t_GVC = fine_GVC - inizio_GVC
+for elem in listGVC:
+    print(elem, end=", ")
+print("\nMin vertex cover: ")
+inizio_MVC = time.clock()
+listMVC = graph2.min_vertex_cover()
 fine_MVC = time.clock()
 t_MVC = fine_MVC - inizio_MVC
 for elem in listMVC:
