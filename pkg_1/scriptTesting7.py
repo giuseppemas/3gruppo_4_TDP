f937aebe3f9e1fc0e72813b11c44ed570a8fcdff
BonnieCharlie <BonnieCharlie@users.noreply.github.com>
2018-01-07 19:52:10 +0100

    aggiunto scriptTesting40

--- pkg_1/scriptTesting7.py
+++ pkg_1/scriptTesting7.py
@@ -5,9 +5,6 @@ e di quanto il vertex cover restituito è più grande.'''
 
 from pkg_1.MyGraph import MyGraph
 import time
-from pkg_2.bridge import randomGraph
-from pkg_2.bridge import connected
-from TdP_collections.graphs.dfs import DFS_complete
 
 print("Testing50 #1: Grafo non diretto non pesato con 51 vertici e 20 archi")
 graph2 = MyGraph()
@@ -58,7 +55,7 @@ for elem in listMVC:
 print("\n\nGREEDY --> Dim =", len(listGVC), ", Time =", t_GVC, "s\n")
 print("MIN --> Dim =", len(listMVC), ", Time =", t_MVC, "s\n")
 print("La dimensione dell'algoritmo Greedy è di", len(listGVC)-len(listMVC), "vertici in più rispetto all'algoritmo Min.\n")
-print("L'algoritmo Greedy è", int(t_MVC/t_GVC), "volte piu' veloce di quello Min.\n")
+print("L'algoritmo Greedy è", t_MVC/t_GVC, "volte piu' veloce di quello Min.\n")
 print("L'algoritmo Greedy impiega", t_MVC-t_GVC, "secondi in meno rispetto a quello Min.\n")
 print("\n\n")
 
@@ -113,7 +110,7 @@ for elem in listMVC:
 print("\n\nGREEDY --> Dim =", len(listGVC), ", Time =", t_GVC, "s\n")
 print("MIN --> Dim =", len(listMVC), ", Time =", t_MVC, "s\n")
 print("La dimensione dell'algoritmo Greedy è di", len(listGVC)-len(listMVC), "vertici in più rispetto all'algoritmo Min.\n")
-print("L'algoritmo Greedy è", int(t_MVC/t_GVC), "volte piu' veloce di quello Min.\n")
+print("L'algoritmo Greedy è", t_MVC/t_GVC, "volte piu' veloce di quello Min.\n")
 print("L'algoritmo Greedy impiega", t_MVC-t_GVC, "secondi in meno rispetto a quello Min.\n")
 print("\n\n")
 
@@ -168,7 +165,7 @@ for elem in listMVC:
 print("\n\nGREEDY --> Dim =", len(listGVC), ", Time =", t_GVC, "s\n")
 print("MIN --> Dim =", len(listMVC), ", Time =", t_MVC, "s\n")
 print("La dimensione dell'algoritmo Greedy è di", len(listGVC)-len(listMVC), "vertici in più rispetto all'algoritmo Min.\n")
-print("L'algoritmo Greedy è", int(t_MVC/t_GVC), "volte piu' veloce di quello Min.\n")
+print("L'algoritmo Greedy è", t_MVC/t_GVC, "volte piu' veloce di quello Min.\n")
 print("L'algoritmo Greedy impiega", t_MVC-t_GVC, "secondi in meno rispetto a quello Min.\n")
 print("\n\n")
 
@@ -237,7 +234,7 @@ for elem in listMVC:
 print("\n\nGREEDY --> Dim =", len(listGVC), ", Time =", t_GVC, "s\n")
 print("MIN --> Dim =", len(listMVC), ", Time =", t_MVC, "s\n")
 print("La dimensione dell'algoritmo Greedy è di", len(listGVC)-len(listMVC), "vertici in più rispetto all'algoritmo Min.\n")
-print("L'algoritmo Greedy è", int(t_MVC/t_GVC), "volte piu' veloce di quello Min.\n")
+print("L'algoritmo Greedy è", t_MVC/t_GVC, "volte piu' veloce di quello Min.\n")
 print("\n\n")
 
 #---------------------------------------------------------------------------------------
@@ -291,7 +288,7 @@ for elem in listMVC:
 print("\n\nGREEDY --> Dim =", len(listGVC), ", Time =", t_GVC, "s\n")
 print("MIN --> Dim =", len(listMVC), ", Time =", t_MVC, "s\n")
 print("La dimensione dell'algoritmo Greedy è di", len(listGVC)-len(listMVC), "vertici in più rispetto all'algoritmo Min.\n")
-print("L'algoritmo Greedy è", int(t_MVC/t_GVC), "volte piu' veloce di quello Min.\n")
+print("L'algoritmo Greedy è", t_MVC/t_GVC, "volte piu' veloce di quello Min.\n")
 print("L'algoritmo Greedy impiega", t_MVC-t_GVC, "secondi in meno rispetto a quello Min.\n")
 print("\n\n")
 
@@ -346,48 +343,9 @@ for elem in listMVC:
 print("\n\nGREEDY --> Dim =", len(listGVC), ", Time =", t_GVC, "s\n")
 print("MIN --> Dim =", len(listMVC), ", Time =", t_MVC, "s\n")
 print("La dimensione dell'algoritmo Greedy è di", len(listGVC)-len(listMVC), "vertici in più rispetto all'algoritmo Min.\n")
-print("L'algoritmo Greedy è", int(t_MVC/t_GVC), "volte piu' veloce di quello Min.\n")
+print("L'algoritmo Greedy è", t_MVC/t_GVC, "volte piu' veloce di quello Min.\n")
 print("L'algoritmo Greedy impiega", t_MVC-t_GVC, "secondi in meno rispetto a quello Min.\n")
 print("\n\n")
-
-#---------------------------------------------------------------------------------------
-
-N = 40
-n = 50
-m = 70
-connessi = 0
-while connessi < N:
-    g1 = randomGraph(n, m)
-
-    if connected(g1):
-        connessi += 1
-        print("GENERAZIONE GRAFO RANDOM: ", connessi + 1, "\n")
-        print("Numero dei vertici: ", n)
-        print("Archi", m, ":")
-        for e in g1.edges():
-            print(e, end=", ")
-
-        print("Greedy vertex cover: ")
-        inizio_GVC = time.clock()
-        listGVC = g1.greedy_vertex_cover()
-        fine_GVC = time.clock()
-        t_GVC = fine_GVC - inizio_GVC
-        for elem in listGVC:
-            print(elem, end=", ")
-        print("\nMin vertex cover: ")
-        inizio_MVC = time.clock()
-        listMVC = g1.min_vertex_cover()
-        fine_MVC = time.clock()
-        t_MVC = fine_MVC - inizio_MVC
-        for elem in listMVC:
-            print(elem, end=", ")
-        print("\n\nGREEDY --> Dim =", len(listGVC), ", Time =", t_GVC, "s\n")
-        print("MIN --> Dim =", len(listMVC), ", Time =", t_MVC, "s\n")
-        print("La dimensione dell'algoritmo Greedy è di", len(listGVC) - len(listMVC),
-              "vertici in più rispetto all'algoritmo Min.\n")
-        print("L'algoritmo Greedy è", int(t_MVC / t_GVC), "volte piu' veloce di quello Min.\n")
-        print("L'algoritmo Greedy impiega", t_MVC - t_GVC, "secondi in meno rispetto a quello Min.\n")
-        print("\n\n")
 
 #---------------------------------------------------------------------------------------
 
@@ -440,6 +398,6 @@ for elem in listMVC:
 print("\n\nGREEDY --> Dim =", len(listGVC), ", Time =", t_GVC, "s\n")
 print("MIN --> Dim =", len(listMVC), ", Time =", t_MVC, "s\n")
 print("La dimensione dell'algoritmo Greedy è di", len(listGVC)-len(listMVC), "vertici in più rispetto all'algoritmo Min.\n")
-print("L'algoritmo Greedy è", int(t_MVC/t_GVC), "volte piu' veloce di quello Min.\n")
+print("L'algoritmo Greedy è", t_MVC/t_GVC, "volte piu' veloce di quello Min.\n")
 print("L'algoritmo Greedy impiega", t_MVC-t_GVC, "secondi in meno rispetto a quello Min.\n")
 print("\n\n")
